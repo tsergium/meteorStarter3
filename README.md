3303ac10ee72f33515741c4a78514d013492b5bc
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-03-30 11:20:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Create new project
+==================
+```bash
+meteor create shorten_my_link
+```
+
+Install React and ReactDOM
+==========================
+```bash
+meteor npm install --save react react-dom
+```
+
+Add Bootstrap
+=============
+```bash
+meteor add twbs:bootstrap@3.3.6
+```
---
+++ shorten_my_link/client/components/link_create.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class LinkCreate extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.refs.link.value);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <div className="form-group">
+                    <label>Link to shorten</label>
+                    <input ref="link" className="form-control" />
+                </div>
+                <button className="btn btn-primary">Shorten!</button>
+            </form>
+        );
+    }
+}
+
+export default LinkCreate;
